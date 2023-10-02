e73f2496af87d37f6850d6ebeab3c7714529247a
Klaus Cinibulk <klaus.cinibulk@rbinternational.com>
2023-10-02 09:49:21 +0200

    refactor: separate update strategy per item type

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,41 +1,3 @@
-# -*- coding: utf-8 -*-
-
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = max(0, item.quality - (2 if item.name == "Conjured Mana Cake" else 1))
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = max(0, item.quality - (2 if item.name == "Conjured Mana Cake" else 1))
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -44,3 +6,42 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class GildedRose(object):
+    def __init__(self, items: list[Item]):
+        self.items = items
+
+    def update_quality(self):
+        for i, item in enumerate(self.items):
+            match item.name:
+                case "Aged Brie": self.update_brie(i)
+                case "Sulfuras, Hand of Ragnaros": self.update_sulfuras(i)
+                case "Backstage passes to a TAFKAL80ETC concert": self.update_backstage_pass(i)
+                case "Conjured Mana Cake": self.update_conjured_item(i)
+                case _: self.update_regular_item(i)
+
+    def _cycle(self, i: int, new_qual, limit_func, limit_val):
+        self.items[i].sell_in -= 1
+        self.items[i].quality = limit_func(new_qual, limit_val)
+
+    def update_regular_item(self, i: int) -> None:
+        reduction = 1 if self.items[i].sell_in > 0 else 2
+        self._cycle(i, self.items[i].quality - reduction, max, 0)
+
+    def update_conjured_item(self, i: int) -> None:
+        reduction = 2 if self.items[i].sell_in > 0 else 4
+        self._cycle(i, self.items[i].quality - reduction, max, 0)
+
+    def update_brie(self, i: int) -> None:
+        reduction = -1 if self.items[i].sell_in > 0 else -2
+        self._cycle(i, self.items[i].quality - reduction, min, 50)
+
+    def update_backstage_pass(self, i: int) -> None:
+        s, q = self.items[i].sell_in, self.items[i].quality
+        reduction = q if s <= 0 else -3 if s <= 5 else -2 if s <= 10 else -1
+        self._cycle(i, self.items[i].quality - reduction, min, 50)
+
+    def update_sulfuras(self, i: int) -> None:
+        self.items[i].quality = 80
+
