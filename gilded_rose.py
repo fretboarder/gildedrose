dd4b849e90512b18ce8d8528371bc208d32a849e
Klaus Cinibulk <klaus.cinibulk@rbinternational.com>
2023-10-02 11:46:05 +0200

    refactor: make the methods pure

--- gilded_rose.py
+++ gilded_rose.py
@@ -15,33 +15,42 @@ class GildedRose(object):
     def update_quality(self):
         for i, item in enumerate(self.items):
             match item.name:
-                case "Aged Brie": self.update_brie(i)
-                case "Sulfuras, Hand of Ragnaros": self.update_sulfuras(i)
-                case "Backstage passes to a TAFKAL80ETC concert": self.update_backstage_pass(i)
-                case "Conjured Mana Cake": self.update_conjured_item(i)
-                case _: self.update_regular_item(i)
+                case "Aged Brie":
+                    self.items[i] = self.update_brie(item)
+                case "Sulfuras, Hand of Ragnaros":
+                    self.items[i] = self.update_sulfuras(item)
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    self.items[i] = self.update_backstage_pass(item)
+                case "Conjured Mana Cake":
+                    self.items[i] = self.update_conjured_item(item)
+                case _:
+                    self.items[i] = self.update_regular_item(item)
 
-    def _cycle(self, i: int, new_qual, limit_func, limit_val):
-        self.items[i].sell_in -= 1
-        self.items[i].quality = limit_func(new_qual, limit_val)
+    @staticmethod
+    def _update(item: Item, reduction: int, limit_func, limit_val) -> Item:
+        name, sell_in, quality = item.__dict__.values()
+        return Item(name, sell_in - 1, limit_func(quality-reduction, limit_val))
 
-    def update_regular_item(self, i: int) -> None:
-        reduction = 1 if self.items[i].sell_in > 0 else 2
-        self._cycle(i, self.items[i].quality - reduction, max, 0)
+    def update_regular_item(self, item: Item) -> Item:
+        reduction = 1 if item.sell_in > 0 else 2
+        return self._update(item, reduction, max, 0)
 
-    def update_conjured_item(self, i: int) -> None:
-        reduction = 2 if self.items[i].sell_in > 0 else 4
-        self._cycle(i, self.items[i].quality - reduction, max, 0)
+    def update_conjured_item(self, item: Item) -> Item:
+        reduction = 2 if item.sell_in > 0 else 4
+        return self._update(item, reduction, max, 0)
 
-    def update_brie(self, i: int) -> None:
-        reduction = -1 if self.items[i].sell_in > 0 else -2
-        self._cycle(i, self.items[i].quality - reduction, min, 50)
+    def update_brie(self, item: Item) -> Item:
+        reduction = -1 if item.sell_in > 0 else -2
+        return self._update(item, reduction, min, 50)
 
-    def update_backstage_pass(self, i: int) -> None:
-        s, q = self.items[i].sell_in, self.items[i].quality
-        reduction = q if s <= 0 else -3 if s <= 5 else -2 if s <= 10 else -1
-        self._cycle(i, self.items[i].quality - reduction, min, 50)
+    def update_backstage_pass(self, item: Item) -> Item:
+        reduction = (
+            item.quality if item.sell_in <= 0
+            else -3 if item.sell_in <= 5
+            else -2 if item.sell_in <= 10
+            else-1
+        )
+        return self._update(item, reduction, min, 50)
 
-    def update_sulfuras(self, i: int) -> None:
-        self.items[i].quality = 80
-
+    def update_sulfuras(self, item: Item) -> Item:
+        return Item(item.name, item.sell_in, 80)
